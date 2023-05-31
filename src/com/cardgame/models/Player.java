3c6bd31fe42dffeb3d67a32d235de02624d7c06d
Shreyas-Nelgikar <nelgikar.works@gmail.com>
2023-06-01 01:19:49 +0530

    seventh commit

--- src/com/cardgame/models/Player.java
+++ src/com/cardgame/models/Player.java
@@ -1,4 +1,25 @@
 package com.cardgame.models;
 
+import java.util.ArrayList;
+
 public class Player {
+
+    private int playerId;
+    private String playerName;
+    private ArrayList<Card> cardsInHand;
+
+    Player (int playerId, String playerName, ArrayList<Card> cardsInHand) {
+        this.playerId = playerId;
+        this.playerName = playerName;
+        this.cardsInHand = cardsInHand;
+    }
+
+    public void addCard (ArrayList<Card> list, Card card) {
+        list.add(card);
+    }
+
+    public void removeCard (ArrayList<Card> list, Card card) {
+        list.remove(card);
+    }
+
 }
