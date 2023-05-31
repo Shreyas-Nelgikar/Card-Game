57149d3de7acd6526ec2a53f012a568e53ed148b
Shreyas-Nelgikar <nelgikar.works@gmail.com>
2023-06-01 01:02:07 +0530

    fifth commit

--- src/com/cardgame/models/Card.java
+++ src/com/cardgame/models/Card.java
@@ -1,4 +1,28 @@
 package com.cardgame.models;
 
 public class Card {
+
+    private int rank;
+    private char suit;
+
+    Card (int rank, char suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public char getSuit() {
+        return suit;
+    }
+
+    public void setSuit(char suit) {
+        this.suit = suit;
+    }
 }
