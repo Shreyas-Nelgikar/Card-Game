5de529a4135906efb32557ea959f372bde505b16
Shreyas-Nelgikar <nelgikar.works@gmail.com>
2023-06-01 11:37:18 +0530

    tenth commit

--- src/com/cardgame/models/Player.java
+++ src/com/cardgame/models/Player.java
@@ -7,6 +7,7 @@ public class Player {
     private int playerId;
     private String playerName;
     private ArrayList<Card> cardsInHand;
+    private final String[] bonus = {"A", "K", "Q", "J"};
 
     Player (int playerId, String playerName, ArrayList<Card> cardsInHand) {
         this.playerId = playerId;
@@ -29,12 +30,24 @@ public class Player {
         System.out.println(arr);
     }
 
-    public boolean hasPlayableCard (ArrayList<Card> list, Card topCard) {
-        for (Card cards : list) {
-            if (topCard.getSuit() == cards.getSuit() || topCard.getRank() == cards.getRank())
-                return true;
+    public void hasBonusCard (Card topCard) {
+        if (topCard.getRank().equals(bonus[0])) {
+
         }
-        return false;
+        else if (topCard.getRank().equals(bonus[1])) {
+
+        }
+        else if (topCard.getRank().equals(bonus[2])) {
+
+        }
+        else if (topCard.getRank().equals(bonus[3])) {
+
+        }
+    }
+
+    public void hasPlayableCard (Card card, Card topCard) {
+        if (card.getRank() != topCard.getRank() || card.getSuit() != topCard.getSuit())
+            System.out.println("Please re-select a Card that matches Rank or Suit of the topCard");
     }
 
     public boolean hasWon (ArrayList<Card> list) {
