94fe5d785d3eaeef3d46de10264f57bde4173731
Shreyas-Nelgikar <nelgikar.works@gmail.com>
2023-06-01 12:45:42 +0530

    twelve commit

--- src/com/cardgame/models/Game.java
+++ src/com/cardgame/models/Game.java
@@ -1,4 +1,49 @@
 package com.cardgame.models;
 
+import java.util.ArrayList;
+
 public class Game {
+
+    private ArrayList<Player> players;
+    private ArrayList<Card> standardDeck;
+    private int currPlayerIndex;
+    private Card topCard;
+
+    Game (ArrayList<Player> players) {
+        this.currPlayerIndex = 0;
+        this.players = players;
+        this.topCard = null;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    public ArrayList<Card> getStandardDeck() {
+        return standardDeck;
+    }
+
+    public void setStandardDeck(ArrayList<Card> standardDeck) {
+        this.standardDeck = standardDeck;
+    }
+
+    public int getCurrPlayerIndex() {
+        return currPlayerIndex;
+    }
+
+    public void setCurrPlayerIndex(int currPlayerIndex) {
+        this.currPlayerIndex = currPlayerIndex;
+    }
+
+    public Card getTopCard() {
+        return topCard;
+    }
+
+    public void setTopCard(Card topCard) {
+        this.topCard = topCard;
+    }
 }
